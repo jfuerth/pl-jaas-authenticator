4b790f9fc4c9ed253df86c5a4c5956e2372f3213
Jonathan Fuerth <jfuerth@gmail.com>
2014-09-17 17:53:39 -0400

    Initial commit. Although it does work, I think it's a suboptimal solution.
    Will work on implementing it as an IdentityStore tomorrow.

---
+++ src/main/java/org/uberfire/extras/pl/PicketLinkJaasSettings.java
@@ -0,0 +1,33 @@
+package org.uberfire.extras.pl;
+
+
+public class PicketLinkJaasSettings {
+
+    public static final String DEFAULT_DOMAIN = "ApplicationRealm";
+    public static final String DEFAULT_ROLE_PRINCIPAL_NAME = "Roles";
+
+    private String domain = DEFAULT_DOMAIN;
+    private String rolePrincipalName = DEFAULT_ROLE_PRINCIPAL_NAME;
+
+    public String getDomain() {
+        return domain;
+    }
+
+
+    public void setDomain( String domain ) {
+        this.domain = domain;
+    }
+
+
+
+    public String getRolePrincipalName() {
+        return rolePrincipalName;
+    }
+
+
+
+    public void setRolePrincipalName( String rolePrincipalName ) {
+        this.rolePrincipalName = rolePrincipalName;
+    }
+
+}
